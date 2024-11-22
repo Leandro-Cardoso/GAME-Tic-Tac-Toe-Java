1b349fc71f9ed6615d3171c30563f19ac561e3c3
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-11-22 01:37:16 -0300

    Class Player

---
+++ Player.java
@@ -0,0 +1,43 @@
+public class Player {
+    private String name;
+    private char symbol;
+    private int score;
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+        this.score = 0;
+    }
+
+    public Player(String name) {
+        this(name, 'X');
+    }
+
+    public Player() {
+        this("IA", 'O');
+    }
+
+    public void win() {
+        this.score++;
+    }
+
+    public void reset() {
+        this.score = 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Player) {
+            Player player = (Player) obj;
+            return this.name == player.name ? true : false;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return " Player (" + this.symbol + "): " + this.name + " - " + this.score;
+    }
+}
